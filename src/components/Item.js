c5ea06de3d60f08223e250d01c5c90dc255306ef
Daniel Molina <dmolcap@gmail.com>
2021-07-16 22:09:21 +0200

    Add clickable checkbox

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,9 +2,26 @@ import React from "react";
 
 function Item(props) {
    const { item } = props;
+   const [isChecked, setIsChecked] = React.useState(false);
+
+   const handleChecking = () => {
+      if (isChecked) {
+         setIsChecked(false);
+      } else {
+         setIsChecked(true);
+      }
+   }
 
    return (
-      <p>{item}</p>
+      <div>
+         <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={() => handleChecking()}
+         >
+         </input>
+         <label>{item}</label>
+      </div>
    );
 }
 
